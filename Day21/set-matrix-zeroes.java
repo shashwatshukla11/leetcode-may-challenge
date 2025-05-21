71ca1b75f1b3e2daa9f21b8050eac24e10a48ded
Shashwat Shukla <167205351+shashwatshukla11@users.noreply.github.com>
2025-05-21 16:09:24 +0530

    Create set-matrix-zeroes.java

---
+++ Day21/set-matrix-zeroes.java
@@ -0,0 +1,29 @@
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] row = new int[m];
+        int[] col = new int[n];
+
+        // First pass to find all rows and columns that need to be zeroed
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        // Second pass to set appropriate elements to zero
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
